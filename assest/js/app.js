c085b4e506b205845129999f17ad0a87bc870195
SajibSamadder <88047138+SajibSamadder@users.noreply.github.com>
2022-07-21 23:43:57 +0600

    First Time

---
+++ assest/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".news_card_wrapper.owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        smartSpeed: 2000,
+        margin: 30,
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Ss")
+})
