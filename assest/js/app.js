a4a5fd24803137913430d71f739b5243f2c1355e
SajibSamadder <88047138+SajibSamadder@users.noreply.github.com>
2022-07-28 12:19:03 +0600

    CSS done

--- assest/js/app.js
+++ assest/js/app.js
@@ -29,4 +29,45 @@ $(document).ready(function() {
         // stagePadding: 250,
 
     });
+    $(".popular_video_box.owl-carousel").owlCarousel({
+        items: 1,
+        center: true,
+        // loop: false,
+        nav: false,
+        autoplay: false,
+        autoplayTimeout: 1000,
+        URLhashListener: true,
+        autoplayHoverPause: true,
+        startPosition: 'video-1',
+        margin: 10,
+        dots: false,
+
+
+
+    });
+    $(".popular_video_url_hash.owl-carousel").owlCarousel({
+        items: 3,
+        autoplay: true,
+        loop: false,
+        nav: false,
+        autoplayTimeout: 3000,
+        smartSpeed: 1000,
+        margin: 20,
+        dots: false,
+
+    });
+
+    $(".modifi_post.owl-carousel.owl-carousel").owlCarousel({
+        items: 1,
+        autoplay: true,
+        loop: false,
+        nav: false,
+        autoplayTimeout: 3000,
+        smartSpeed: 1000,
+        margin: 10,
+        dots: true,
+
+    });
+
+
 })
